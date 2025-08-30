e77b8612be807454d7173da6026af176845bf9ad
jeetCl <devsecopsjeet@gmail.com>
2025-08-31 04:02:33 +0530

    Create XSSController.java

---
+++ AntSpringMVC/src/com/mkyong/controller/controller/XSSController.java
@@ -0,0 +1,16 @@
+package com.mkyong.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class XSSController {
+
+    public void vulnerableXSS(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String name = request.getParameter("name"); // unsafe input
+        PrintWriter out = response.getWriter();
+
+        // Vulnerable: output user input directly to page
+        out.println("<h1>Hello " + name + "</h1>");
+    }
+}
