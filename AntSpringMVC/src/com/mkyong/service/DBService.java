82d5190f05308daf5f673b4dbb519ecd22314382
jeetCl <devsecopsjeet@gmail.com>
2025-08-31 09:14:40 +0530

    Create DBService.java

---
+++ AntSpringMVC/src/com/mkyong/service/DBService.java
@@ -0,0 +1,14 @@
+package com.mkyong.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBService {
+
+    public Connection getConnection() throws Exception {
+        // Vulnerable: hardcoded credentials
+        String username = "admin";
+        String password = "admin123";
+        return DriverManager.getConnection("jdbc:h2:mem:testdb", username, password);
+    }
+}
